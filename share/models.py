37e4f9981a12658b53c4a8418b39fbb2c7dda07a
Jhsysng <ysng1118@naver.com>
2022-08-23 22:57:39 +0900

    mtv bone

--- share/models.py
+++ share/models.py
@@ -1,6 +1,29 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
 #now testing
-class ShareImage(models.Model):
-    test=1
+
+class ShareFile(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.DO_NOTHING)
+    name = models.CharField(max_length=40)
+    upload_date = models.DateTimeField(default=timezone.now)
+
+
+class Photo(ShareFile):
+    photo = models.ImageField(upload_to="/")
+    pass
+class File(ShareFile):
+    pass
+
+class Video(ShareFile):
+    pass
+
+class ShareAlbum(models.Model):
+    model=models.ForeignKey(ShareFile,on_delte=models.CASCADE)
+
+
+
+
+
 # Create your models here.
