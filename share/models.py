f944dbb85cc39b2f8e9da9e2e9d4e62e7e163c84
Jhsysng <ysng1118@naver.com>
2022-08-31 20:09:01 +0900

    model - 1 안

--- share/models.py
+++ share/models.py
@@ -2,28 +2,25 @@ from django.db import models
 from django.conf import settings
 from django.utils import timezone
 from hashlib import sha256
-#from apps import pilling
 from PIL import Image
 import os
 from random import random
 import pillow_heif
 import googlemaps
 
-#now testing
+# now testing
+
 
 def set_path(instance):
     return "%s"%instance.album_id
 
-class ShareFile(models.Model):
 
-    #__new__
-    author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.DO_NOTHING)
-    album_id = models.ForeignKey(models.ShareAlbum,on_delte=models.CASCADE)
+class ShareFile(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING)
+    album_id = models.ForeignKey(models.ShareAlbum, on_delte=models.CASCADE)
     name = models.CharField(max_length=40)
     upload_date = models.DateTimeField(default=timezone.now)
-
-    def set_upload(self):
-        return "share/"+self.album_id
+    file_size = models.IntegerField(max_length=10)
 
     class Meta:
         abstract = True
@@ -31,28 +28,26 @@ class ShareFile(models.Model):
 
 class Photo(ShareFile):
     # field (author room name upload_date gps)
-    photo = models.ImageField(upload_to = "share/"+set_path+"/photo/")
-    gps = models.CharField()
+    photo = models.ImageField(upload_to="share/"+set_path+"/photo/")
+    gps_addr = models.CharField(max_length=20)
+
 
     pass
+
+
 class File(ShareFile):
-    file=models.FileField(upload_to="share/"+set_path+"/photo/")
+    file = models.FileField(upload_to="share/"+set_path+"/photo/")
     ext = models.CharField(max_length=8)
 
 
-
-
-
-
 class Video(ShareFile):
-    video = models.FileField(upload_to = "share/"+set_path+"/video")
+    video = models.FileField(upload_to="share/"+set_path+"/video")
+
 
 class ShareAlbum(models.Model):
+    album_admin = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     album_id = models.CharField(max_length=20)
-
-
-
-
+    storage = models.IntegerField(max_length=10)
 
 
 # Create your models here.
