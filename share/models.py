f73ba7c625272c55c40301f1c0b8e49c38c98568
Han Junho <datah07301@gamil.com>
2022-09-08 23:38:20 +0900

    submain2 ver 0.1

--- share/models.py
+++ share/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+def set_path(instance):
+    return "%s" %(instance.album_id)
+
+
+class ShareFile(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.DO_NOTHING)
+    room = models.ForeignKey(models.ShareAlbum, on_delte=models.CASCADE)
+    name = models.CharField(max_length=40)
+    upload_date = models.DateTimeField(default=timezone.now)
+    file_size = models.IntegerField(max_length=10)
+
+    class Meta:
+        abstract = True
+
+
+class Photo(ShareFile):
+    # field (author room name upload_date gps)
+    photo = models.ImageField(upload_to="share/photo/")
+    gps_addr = models.CharField(max_length=20, blank=True)
+    pixel = models.CharField(max_length=20)
+    ocr_tag = models.SlugField(blank=True)
+
+class File(ShareFile):
+    file = models.FileField(upload_to="share/file/")
+    ext = models.CharField(max_length=8)
+
+class Video(ShareFile):
+    video = models.FileField(upload_to="share/video/")
+    gps_addr = models.CharField(max_length=20, blank=True)
+
+class ShareAlbum(models.Model):
+    album_admin = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    share_id = models.CharField(max_length=20)
+    storage = models.IntegerField(max_length=10)
+
+
+
+
 
 # Create your models here.
