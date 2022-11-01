61ded1c2d0a253e6310268f1ecf5f59f506b6b32
Jhsysng <ysng1118@naver.com>
2022-11-01 20:39:56 +0900

    form teul

--- share/forms.py
+++ share/forms.py
@@ -4,3 +4,19 @@ from django import forms
 class PhotoForm(forms.ModelForm):
     file_field = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
 
+    class Meta:
+        model = File
+        fields = ["name", "photo", "gps_addr", "tag",]
+class FileForm(forms.ModelForm):
+    file_field = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
+
+    class Meta:
+        model = Photo
+        fields = ["name", "file", "ext", ]
+class VideoForm(forms.ModelForm):
+    file_field = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple':True}))
+
+    class Meta:
+        model = Video
+        fields = ["name", "video","gps_addr", ]
+
--- share/views.py
+++ share/views.py
@@ -2,10 +2,27 @@ from django.shortcuts import render
 from django.views.generic.edit import FormView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, FormView
-from .forms import PhotoForm
+from .forms import FileForm, PhotoForm, VideoForm
 from .models import Photo, Video, File, ShareAlbum
 import os
 class FileFieldFormView(LoginRequiredMixin, FormView):
+    form_class = FileForm
+    template_name = 'upload.html'  # Replace with your template.
+    success_url = ''#reverse()  # Replace with your URL or reverse(). #album redirect
+    def post(self, request, *args, **kwargs):
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        files = request.FILES.getlist('file_field')
+        if form.is_valid():
+            for f in files:
+
+
+                ...  # Do something with each file.
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+class PhotoFieldFormView(LoginRequiredMixin, FormView):
     form_class = PhotoForm
     template_name = 'upload.html'  # Replace with your template.
     success_url = ''#reverse()  # Replace with your URL or reverse(). #album redirect
@@ -15,12 +32,11 @@ class FileFieldFormView(LoginRequiredMixin, FormView):
         files = request.FILES.getlist('file_field')
         if form.is_valid():
             for f in files:
+                name=f
                 ...  # Do something with each file.
             return self.form_valid(form)
         else:
             return self.form_invalid(form)
-
-
 # Create your views here.
 class AlbumView(LoginRequiredMixin, ListView):
     model = ShareAlbum
